b86709676cc80e4adcf4731022df282148133882
Amlindmae <113936984+ALindmae@users.noreply.github.com>
2025-09-26 12:32:40 -0300

    Create getComputerChoice function for rock, paper, scissors game

---
+++ script.js
@@ -0,0 +1,25 @@
+// Create function that returns "rock", "paper" or "scissors" at random
+
+function getComputerChoice () {
+    let choices = 3;
+    let randomPick = Math.floor(Math.random() * choices);
+    switch (randomPick) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+    
+}
+
+
+// Prompt human rock, paper, scissors decision
+
+// Keep track of players score
+
+// Play a round = take both decisions into account and decide who wins  
+
+// game will play 5 rounds
+
